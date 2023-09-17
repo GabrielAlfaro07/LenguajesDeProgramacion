d05a219acc696b822d9a3199e51e5c14983f0f17
GabrielAlfaro07 <galfaulate@gmail.com>
2023-09-16 18:43:11 -0600

    Add excercise 3 and 5

---
+++ EjerciciosFuncionales/Practica1/Ejercicio3/Ejercicio3/Program.fs
@@ -0,0 +1,17 @@
+﻿let mapPosition position list =
+    List.mapi (fun index x -> if index = position then x else -1) list
+
+let filterNotMinusOne list =
+    List.filter (fun x -> x <> -1) list
+
+let n_esimo position list = 
+    if position >= List.length list then -1
+    else
+       let filteredList = mapPosition position list
+       let filteredResult = filterNotMinusOne filteredList
+       filteredResult.[0]
+   
+let numbers = [1; 2; 3; 4; 5]
+let result = n_esimo 0 numbers
+
+printfn "Result: %d" result
